4ad114d37a8c55f2f606070ac3900c5959355a3e
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2021-10-05 08:45:54 -0400

    Update submission scripts

--- scripts/month-AG08.sh
+++ scripts/month-AG08.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu 8G
 #SBATCH --job-name=AG08-month
 #SBATCH --output=./outputs/%x-%j.out
-#SBATCH --dependency=afterok:52917595
+#SBATCH --dependency=afterok:52947825
 
 #---
 #title: "PCNM for Squirrel Microbiome (SHARCNET)"
