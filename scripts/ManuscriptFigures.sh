afe60a33885f33ed1b1ebfd8310e45b188018b4e
Alicia Halhed <55498260+ahalhed@users.noreply.github.com>
2022-08-09 16:24:02 -0400

    Update ManuscriptFigures.sh

--- scripts/ManuscriptFigures.sh
+++ scripts/ManuscriptFigures.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=def-cottenie
+#SBATCH --account=def-rowlando
 #SBATCH --time=0-00:45:00
 #SBATCH --mem-per-cpu 8G
 #SBATCH --job-name=Figures
@@ -9,4 +9,4 @@
 module load nixpkgs/16.09 gcc/7.3.0 r/3.6.0
 
 # run R script
-Rscript /home/ahalhed/projects/def-cottenie/Microbiome/RedSquirrelMicrobiome/Red-Squirrel-Microbiome/scripts/ManuscriptFigures.R
+Rscript /home/ahalhed/MSc/Red-Squirrel-Microbiome/scripts/ManuscriptFigures.R
